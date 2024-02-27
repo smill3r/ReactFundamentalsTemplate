2b414495e09d23632440ce3ddd274319fffc3b2e
‘josesmiller’ <josesmiller@google.com>
2024-02-27 11:07:55 -0600

    Add header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import styles from "./styles.module.css";
+import { Button } from "../../common";
+import { Logo } from "./components";
 
 // Module 1:
 // * add Logo and Button components
@@ -36,10 +38,10 @@ export const Header = () => {
 
   return (
     <div className={styles.headerContainer}>
-      // use Logo component
+      <Logo></Logo>
       <div className={styles.userContainer}>
         <p className={styles.userName}>Harry Potter</p>
-        // reuse Button component for 'Login / Logout' button
+        <Button buttonText="LOGIN"></Button>
       </div>
     </div>
   );
