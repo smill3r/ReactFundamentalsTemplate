55b979a215ee064fa635d6e7a68dbf162ef179db
‘josesmiller’ <josesmiller@google.com>
2024-02-27 11:02:21 -0600

    Add helper functions

--- src/helpers/formatCreationDate.js
+++ src/helpers/formatCreationDate.js
@@ -1,5 +1,6 @@
 export const formatCreationDate = (date) => {
   // write your solution here
+  const formattedDate = date.replace(/\//g, ".");
 
-  return date;
+  return formattedDate;
 };
--- src/helpers/getCourseDuration.js
+++ src/helpers/getCourseDuration.js
@@ -1,5 +1,12 @@
 export const getCourseDuration = (duration) => {
   // write your solution here
+  let hours = Math.floor(duration / 60);
+  let minutes = duration % 60;
 
-  return duration;
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  const suffix = hours > 1 ? ` hours` : ` hour`;
+
+  return `${hours}:${minutes + suffix}`;
 };
